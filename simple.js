ce728634446f4f55d2e6402e57349453435e9904
Mikael Tylmad <mikael@roboro.se>
2012-04-26 12:45:39 +0200

    simple.js is now dependant on advanced.js

--- simple.js
+++ simple.js
@@ -1,458 +1,83 @@
 window.onload = init;
 
-function start(){}
-function update(){}
-
-up    = 38;
-down  = 40;
-left  = 37;
-right = 39;
-space = 32;
-zero  = 48;
-one   = 49;
-two   = 50;
-three = 51;
-four  = 52;
-five  = 53;
-six   = 54;
-seven = 55;
-eight = 56;
-nine  = 57;
-w     = 87;
-a     = 65;
-s     = 83;
-d     = 68;
-
-function init()
+function loadScript(url, callback)
 {
-  canvas = document.getElementById('canvas');
-  if (!canvas)
-  {
-    canvas = document.createElement('canvas');
-    canvas.id = 'canvas';
-    canvas.style.position = 'absolute';
-    canvas.style.left = '0';
-    canvas.style.top = '0';
-    document.body.appendChild(canvas);
-  }
-
-  context2D = canvas.getContext('2d');
-
-  totalWidth = canvas.width;
-  totalHeight = canvas.height;
-
-  pi = Math.PI;
-
-  keyboard = new Array();
-  mouse    = new Array();
-  
-  currentKey = "";
-  mouseX = -10000;
-  mouseY = -10000;
-  
-  mouseLeftDown   = false;
-  mouseMiddleDown = false;
-  mouseRightDown  = false;
-  
-  document.onkeydown  = keyPressed;
-  document.onkeyup    = keyReleased;
-  canvas.onmousedown  = mouseDown;
-  canvas.onmouseup    = mouseUp;
-  canvas.onmousemove  = mouseMovement;
-  canvas.ontouchstart = touchDown;
-  canvas.ontouchend   = touchUp;
-  canvas.ontouchmove  = touchMove;
-
-  FPS = 30;
-
-  suppressErrors = false;
-
-  maximizeCanvas();
-  
-  start();
-  
-  theAnimation = setInterval(update, 1000/FPS);
-}
-
-function stopUpdate()
-{
-  clearInterval(theAnimation);
-}
-
-function touchDown(e) 
-{
-  mouseLeftDown = true;
-}
-
-function touchUp(e) 
-{
-  mouseLeftDown = false;
-}
-
-function touchMove(e)
-{
-  if (e.touches.length == 1)
-  {
-    mouseX = e.touches[0].clientX;
-    mouseY = e.touches[0].clientY;
-  }
-  return false;
-}
-
-function mouseDown(event)
-{
-  mouse[event.button] = 1;
-  
-  if (event.button == 0)
-    mouseLeftDown = true;
-  else if (event.button == 1)
-    mouseMiddleDown = true;
-  else if (event.button == 2)
-    mouseRightDown = true;
-}
-
-function mouseUp(event)
-{
-  mouse[event.button] = 0;
-
-  if (event.button == 0)
-    mouseLeftDown = false;
-  else if (event.button == 1)
-    mouseMiddleDown = false;
-  else if (event.button == 2)
-    mouseRightDown = false;
-}
-
-function keyDown(key)
-{
-  return keyboard[key] == 1;
-}
-
-function keyPressed(event)
-{
-  keyboard[event.keyCode] = 1;
-}
-
-function keyReleased(event)
-{
-  keyboard[event.keyCode] = 0;
-}
-
-function mouseMovement(event)
-{
-  if (event.pageX || event.pageY)
-  {
-    pageX = event.pageX;
-    pageY = event.pageY;
-  }
-  else if (event.clientX || event.clientY)
-  {
-    pageX = event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-    pageY = event.clientY + document.body.scrollLeft + document.documentElement.scrollLeft;
-  }
-
-  mouseX = pageX - canvas.offsetLeft;
-  mouseY = pageY - canvas.offsetTop;
-}
-
-function circle(x, y, r, color) 
-{
-  context2D.fillStyle = color;
-  context2D.beginPath();
-  context2D.arc(x, y, r, 0, Math.PI * 2, true);
-  context2D.closePath();
-  context2D.fill();
-}
-
-function rectangle(x, y, width, height, color) 
-{
-  context2D.fillStyle = color;
-  context2D.fillRect(x, y, width, height);
-}
-
-function triangle(x1, y1, x2, y2, x3, y3, color) 
-{
-  context2D.fillStyle = color;
-  context2D.beginPath();
-  context2D.moveTo(x1, y1);
-  context2D.lineTo(x2, y2);
-  context2D.lineTo(x3, y3);
-  context2D.fill();
-}
-
-function text(text, x, y, color, size)
-{
-  context2D.font = size + "pt Helvetica";
-  context2D.fillStyle = color;
-  context2D.fillText(text, x, y);
-}
-
-function random(max)
-{
-  return Math.floor(Math.random() * max);
-}
-
-function randomAlternative(lista)
-{
-  return lista[random(lista.length)];
-}
-
-function picture(x, y, file)
-{
-  img = new Image();
-  img.src = file;
-  context2D.drawImage(img, x, y);
-}
-
-function clearScreen()
-{
-  context2D.clearRect(0, 0, totalWidth, totalHeight);
-}
-
-function clearScreenWithColor(color)
-{
-  clearScreen();
-  rectangle(0, 0, totalWidth, totalHeight, color);
-}
-
-function distance(x1, y1, x2, y2)
-{
-  xdiff = Math.abs(x1 - x2);
-  ydiff = Math.abs(y1 - y2);
-
-  return Math.sqrt(xdiff * xdiff + ydiff * ydiff);
-}
-
-function save()
-{
-  context2D.save();
-}
-
-function restore()
-{
-  context2D.restore();
-}
-
-function translate(x, y)
-{
-  context2D.translate(x, y);
-}
-
-function rotate(degrees)
-{
-  context2D.rotate(degrees * Math.PI / 180);
-}
-
-function rotateRadians(radians)
-{
-  context2D.rotate(radians);
-}
-
-function rgb(r, g, b)
-{
-  return "rgb(" + r + "," + g + "," + b + ")";
-}
-
-function line(x1, y1, x2, y2, width, color)
-{
-  context2D.strokeStyle = color;
-  context2D.lineWidth   = width;
-  context2D.beginPath();
-  context2D.moveTo(x1, y1);
-  context2D.lineTo(x2, y2);
-  context2D.stroke();
-  context2D.closePath();
-}
-
-function sin(angle)
-{
-  return Math.sin(angle);
-}
-
-function asin(angle)
-{
-  return Math.asin(angle);
-}
-
-function cos(angle)
-{
-  return Math.cos(angle);
-}
-
-function acos(angle)
-{
-  return Math.acos(angle);
-}
-
-function tan(angle)
-{
-  return Math.tan(angle);
-}
-
-function atan(angle)
-{
-  return Math.atan(angle);
-}
-
-function sqrt(x)
-{
-  return Math.sqrt(x);
-}
-
-function abs(x)
-{
-  return Math.abs(x);
-}
-
-function pow(x, y)
-{
-  return Math.pow(x, y);
-}
-
-function floor(x)
-{
-  return Math.floor(x);
-}
-
-function emptyCircle(x, y, r, lineWidth, color)
-{
-  context2D.beginPath();
-  context2D.arc(x, y, r, 0, Math.PI * 2, true);
-  context2D.closePath();
-  context2D.lineWidth = lineWidth;
-  context2D.strokeStyle = color;
-  context2D.stroke();
-}
-
-function ring(x, y, r, lineWidth, color)
-{
-  emptyCircle(x, y, r, lineWidth, color);
-}
-
-function emptyArc(x, y, r, angle, lineWidth, color)
-{
-  context2D.beginPath();
-  context2D.arc(x, y, r, 0, -angle, true);
-  context2D.lineWidth = lineWidth;
-  context2D.strokeStyle = color;
-  context2D.stroke();
+  var head = document.getElementsByTagName('head')[0];
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = url;
+  script.onreadystatechange = callback;
+  script.onload = callback;
+  head.appendChild(script);
 }
 
 function maximizeCanvas()
 {
-  canvas.width = window.innerWidth-1;
-  canvas.height = window.innerHeight-1;
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  canvas.style.width = window.innerWidth;
+  canvas.style.height = window.innerHeight;
   totalWidth = canvas.width;
   totalHeight = canvas.height;
 }
 
-function padString(subject, character, length, side)
+function init()
 {
-  subject = "" + subject;
-  while (subject.length < length)
-    subject = side == "left" ? character + subject : subject + character;
-  return subject;
-}
+  canvas = document.createElement('canvas');
+  canvas.id = 'canvas';
+  canvas.style.position = 'absolute';
+  canvas.style.left = '0';
+  canvas.style.top = '0';
+  document.body.setAttribute("oncontextmenu", "return false");
+  document.body.appendChild(canvas);
+  
+  maximizeCanvas();
 
-function logger(message)
-{
-  if (suppressErrors)
-    return false;
+  var env = this;
 
-  var log = document.getElementById('log');
-  
-  var date = new Date();
-  
-  var time = [ padString(date.getHours(), '0', 2, 'left'),
-               padString(date.getMinutes(), '0', 2, 'left'),
-               padString(date.getSeconds(), '0', 2, 'left') ].join(':');
-  
-  if (!log)
-  {
-    log = document.createElement('table');
-    log.id = 'log';
-    log.style.border = '5px outset white';
-    log.style.background = 'black';
-    log.style.color = 'white';
-    log.style.margin = '1em';
-    document.body.appendChild(log);
-  }
-  
-  var row = log.insertRow(-1);
-  row.innerHTML =
-    '<td style="vertical-align: top"><pre>'+ time +
-    '  </pre></td><td><pre>'+
-    message.replace(/</, '&lt;') +'</pre></td>';
-}
+  var simplify = function() {
 
-window.onerror = function(message, url, line)
-{
-  logger('ERROR: '+ message +' (line '+ line +')');
-};
-
-function getSoundChannel()
-{
-  var soundEffects = document.getElementById('soundEffects');
-  
-  if (!soundEffects)
-  {
-    soundEffects = document.createElement('div');
-    soundEffects.id = 'soundEffects';
+    c = new RoboroCanvas('canvas');
     
-    for (var i = 0; i < 8; i++)
-      soundEffects.appendChild(document.createElement('audio'));
+    env.FPS = 30;
     
-    document.body.appendChild(soundEffects);
-  }
-  
-  var channels = soundEffects.children;
-  
-  if (typeof(currentChannel) == 'undefined' || ++currentChannel >= channels.length)
-    currentChannel = 0;
-  
-  return channels[currentChannel];
-}
+    env.mouse = c.mouse;
 
-function preloadSound(sound)
-{
-  if (! sound.match(/\/|\.[a-z]+$/i))
-    sound = 'http://spelprogrammering.nu/sounds/' + sound + '.mp3';
-  
-  var channel = getSoundChannel();
-  channel.preload = 'auto';
-  channel.src = sound;
-}
+    functions = ["circle", 
+                 "rectangle", 
+                 "triangle", 
+                 "ring", 
+                 "arc",
+                 "text",
+                 "random",
+                 "randomAlternative",
+                 "picture",
+                 "clearScreen",
+                 "fill",
+                 "distance",
+                 "save",
+                 "restore",
+                 "translate",
+                 "rotate",
+                 "rotateRadians",
+                 "line"];
 
-function playSound(sound, volume)
-{
-  if (! sound.match(/\/|\.[a-z]+$/i))
-    sound = 'http://spelprogrammering.nu/sounds/' + sound + '.mp3';
-  
-  if (typeof(volume) == 'undefined')
-    var volume = 1;
-  
-  var channel = getSoundChannel();
-  channel.volume = volume;
-  channel.src = sound;
-  channel.play();
-}
+    for (i = 0; i < functions.length; i++)
+      eval("env." + functions[i] + " = function() { c." + functions[i] + ".apply(c, arguments) }");
 
-function playBackgroundSound(sound, volume)
-{
-  var channel = document.getElementById('backgroundSound');
-  
-  if (!channel)
-  {
-    channel = document.createElement('audio');
-    channel.id = 'backgroundSound';
-    channel.loop = true;
-    document.body.appendChild(channel);
-  }
-  
-  if (typeof(volume) == 'undefined')
-    var volume = 1;
-  
-  channel.volume = volume;
-  channel.src = sound;
-  channel.play();
+    if (typeof(start) != "undefined") 
+      start();
+    if (typeof(update) != "undefined") 
+    {
+      updateTimer = setInterval(update, 1000/env.FPS);
+    }
+
+    env.stopUpdate = function()
+    {
+      clearInterval(updateTimer);
+    }
+
+  };
+
+  loadScript("http://www.spelprogrammering.nu/advanced.js", simplify);
 }
